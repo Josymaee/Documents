511013f51c78a3e776f4364a308fb05fba600133
Josymaee <joslynn.ramirez@ucas-edu.net>
2025-02-03 11:43:22 -0700

    expressions

--- Variables/notes.c
+++ Variables/notes.c
@@ -5,6 +5,14 @@ int age = 15;
 float pi = 3.14;
 
 int main(void){
-printf("Hello I am %s I am %d years old and I like %g")
+printf("Welcome, whatis your name: \n");
+scanf("%s",name);
+printf("How old are you: \n");
+scanf("%d", age")
+printf (Write out as much of pi do you know:)
+printf("Hello I am %s I am %d years old. I like the number 
+printf("%d\n", age);
+printf(%f\n", age);
+printf
 
 }

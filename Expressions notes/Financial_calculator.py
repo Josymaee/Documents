39789f3dbbc02c4544e6ab93854b25ccd781ddcd
Josymaee <joslynn.ramirez@ucas-edu.net>
2025-02-06 11:49:01 -0700

    financial calculator

--- Expressions notes/Financial_calculator.py
+++ Expressions notes/Financial_calculator.py
@@ -1,5 +1,29 @@
 # Josy Ramirez, Financial calculator
 
 # print statement that welcomes what the program does
+print("Hello!This is financial calulator to help keep track of your money")
+# ask what their income is (variable an input)
+income = float(input("What is your income?\n"))
+# ask what their rent is (variable an input)
+rent = float(input("How much is your rent?\n"))
+# ask what their utilities is (variable an input)
+utilities = float(input("How much is your utilities?\n"))
+# ask what their groceries is (variable an input)
+groceries = float(input("How much are your groceries?\n"))
+# ask what their transportation is (variable an input)
+transportation = float(input("How much is your transportation?\n"))
+# calculate savings as 10% of income (income*,1) (variable)
+# calculate spending as income-savings-rent-utilites-groceries-transportation (variable)
+# calculate percent income of rent (rent/income*100) (variable)
 
-# 
+# calculate percent income of utilities (utilities/income*100) (variable)
+# calculate percent income of groceries (groceries/income*100) (variable)
+# calculate percent income of transportation (transportation/income*100) (variable)
+# calculate percent income of spending (spending/income*100) (variable)
+
+#Your rent is $XX.XX which is XX% of your income. (Print)
+#Your utilities is $XX.XX which is XX% of your income. (Print)
+#Your groceries is $XX.XX which is XX% of your income. (Print)
+#Your transportation is $XX.XX which is XX% of your income. (Print)
+#Your savings is $XX.XX which is XX% of your income. (Print)
+#Your spending is $XX.XX which is XX% of your income. (Print)

591d3f9752502b98a6ec423ae979eff4ad95a876
Josymaee <joslynn.ramirez@ucas-edu.net>
2025-02-11 11:48:44 -0700

    everything

--- strings/notes.py
+++ strings/notes.py
@@ -7,7 +7,11 @@ name = input("What is your first name?\n")
 
 #print(f"Hello {name}, welcome to my program.").strip().capitalize()
 
-print(f"this is your name"+ name)
+# print(f"this is your name"+ name)
 
 sentence = "The quick brown fox jumps over the lazy dog"
 
+# print(len(sentence ))
+# print(sentence[4])
+print(sentence.find("brown"))
+print(sentence[start:+length])

cb67631997209ce951802bf3ec71e112e840bbfc
Josymaee <joslynn.ramirez@ucas-edu.net>
2025-02-12 11:37:38 -0700

    everythingg

--- Variables/first_program.c
+++ Variables/first_program.c
@@ -1,4 +1,4 @@
-// Josy Ramirez, yes
+// Josy Ramirez, First Program C
 
 #include <studio.h>
 
@@ -7,7 +7,7 @@ int age = 15;
 float pi = 3.14;
 
 int main(void){
-    printf("Hello i am %s, I am %d years old. I like the number %f. \", name);
+    printf("Hello i am %s, I am %d years old. I like the number %f. \n", name);
     printf("\n", age);
     printf("%f\n", pi);
     return 0;

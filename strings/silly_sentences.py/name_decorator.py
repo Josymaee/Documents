eaa18d655de553c93ab1d40947b62ddaa702e43c
Josymaee <joslynn.ramirez@ucas-edu.net>
2025-02-21 11:23:39 -0700

    done

---
+++ strings/silly_sentences.py/name_decorator.py
@@ -0,0 +1,22 @@
+# Josy Ramirez, name decorator python
+
+str_one = "Would you like your name in a different decoration"
+str_two = "If yes, type your name again, if no you can leave the program"
+
+# Welcoming the user
+print("Hello! Thi is a program that makes your name look cool!.")
+
+#Ask for name 
+name = input("What is your name?\n").strip().upper()
+
+print("==",name,"==")
+
+name = input("What is your name?\n").strip().upper()
+
+print("(:(:",name,":):)")
+
+name = input("What is your name?\n").strip().upper()
+
+print("<<<", name "<<<")
+
+print("Thank you for using my program!:)")

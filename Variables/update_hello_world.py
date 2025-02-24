62dd7d527b36b963628ff105a800460d503a7cfa
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-02-23 21:42:25 -0700

    Create update_hello_world.py

---
+++ Variables/update_hello_world.py
@@ -0,0 +1,16 @@
+// joslynn ramirez, update hello world python
+
+#include <stdio.h>
+
+void name(char name[]){
+    printf("hello %s\n", name);
+}
+
+int main(void){
+    name("Emma");
+    name("Cooper");
+    name("Sawyer");
+    name("Nate");
+    name("Chris Hemsworth");
+    return 0;
+}

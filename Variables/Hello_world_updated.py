600fe820c0b7b39763229a59b7bd72bbd3e3b6c0
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-02-23 21:34:48 -0700

    Create Hello_world_updated.py

---
+++ Variables/Hello_world_updated.py
@@ -0,0 +1,9 @@
+# josy ramirez, hello world updated python
+
+def hello (name): 
+    return f"hello{name}"
+print("Hello(Josy)")
+print("Hello(Juan)")
+print("Hello(david)")
+print("Hello(yenesis)")
+print("Hello(Isaac)")

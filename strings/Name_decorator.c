4cd651de400b346f39266e3f408c2c596a22b23d
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-02-23 21:12:14 -0700

    Create Name_decorator.c

---
+++ strings/Name_decorator.c
@@ -0,0 +1,12 @@
+#Joslynn Ramirez, name decorator c
+
+#include <stdio.h>
+
+int main(void){
+char name[30];
+printf("What is your name?: ");
+scanf("%s", name); 
+printf("==%s==", name);
+return 0;
+
+}

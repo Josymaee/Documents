dd2e61e41099e6e48e777a97102f2db64fb972bd
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-02-23 21:24:49 -0700

    Create silly_billy.c

---
+++ strings/silly_billy.c
@@ -0,0 +1,10 @@
+# josy Ramirez, silly sentences c
+
+animal= input ("Name an animal")
+place = input ("Name a place ")
+verb = input ("Name a verb")
+
+print("The", animal)
+print("went to the " ,place)
+print("and it",verb)
+print("Whith thier friend the loud laughing donkey")

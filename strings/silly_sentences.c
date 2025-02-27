200ba0fbd5d6f978b6cb412bfb7669f8f112e4e3
JossBoss <joslynn.ramirez@ucas-edu.net>
2025-02-26 18:58:27 -0700

    Update silly_sentences.c

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -1,29 +1,30 @@
-# Joslynn Ramirez, Silly sentences c
+// Joslynn Ramirez, Silly Sentences in C
 
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
-char animal[20];
-char place[20];
-char verb[20];
-char sentence[400]= "The ";
-        
-printf("Name an animal: ");
-scanf("%s", animal);
-printf("Name a place: ");
-scanf("%s", place);
-printf("Name a past tense verb: ");
-scanf("%s", verb);
-strcat(sentence, animal);
-strcat(sentence, " went to the ");
-strcat(sentence, place);
-strcat(sentence, " and ");
-strcat(sentence, verb);
-strcat(sentence, " with his friend the mean dog. \n");
+int main(void) {
+    char animal[20];
+    char place[20];
+    char verb[20];
+    char sentence[400] = "The ";
+
+    printf("Name an animal: ");
+    scanf("%19s", animal);  
+    printf("Name a place: ");
+    scanf("%19s", place);
+    printf("Name a past tense verb: ");
+    scanf("%19s", verb);
+
+    strcat(sentence, animal);
+    strcat(sentence, " went to the ");
+    strcat(sentence, place);
+    strcat(sentence, " and ");
+    strcat(sentence, verb);
+    strcat(sentence, " with his friend, the mean dog.\n");
+
     printf("%s", sentence);
+
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 022b2abba70bf4a2bbd8650d4c99b7c9649dc1fe
+
